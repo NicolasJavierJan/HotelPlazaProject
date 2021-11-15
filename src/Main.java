207c9f5cdd39f193320c33cb156305240504f676
NicolasJavierJan <66181443+NicolasJavierJan@users.noreply.github.com>
2021-11-15 09:40:34 +0100

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    // Classes:
+    // TODO JASMIN
+    // Room (change price, booking)
+    // Bookings (start date, end date, room number, Guest)
+
+
+    // TODO ANDREA
+    // Guest
+    // Staff (change name, String title (director, cleaning person, etc.), pn and salary)
+    // HUMAN-PERSON-BEING
+
+    // TODO NICO
+    // Make a MENU:
+    // Create ROOM
+    // Create Guest
+    // Create Staff
+    // Create Booking
+    // ENDPROGRAM
+
+}

9d3b2b595f864ff23b45e01ab29a6dcf98cadae9
NicolasJavierJan <nicolasjavierjan@gmail.com>
2021-11-22 11:26:09 +0100

    Changes in Main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
     // Classes:
@@ -18,5 +21,61 @@ public class Main {
     // Create Staff
     // Create Booking
     // ENDPROGRAM
+
+    public static void main(String[] args) {
+
+        boolean keepAsking = true;
+
+        while (keepAsking) {
+            System.out.println("This is the Main Menu");
+            System.out.println("Press 1 to create a Booking.");
+            System.out.println("Administration options:");
+            System.out.println("Press 2 for creating a new Guest");
+            System.out.println("Press 3 for adding a new Room");
+            System.out.println("Press 4 for adding new Staff");
+            System.out.println("Press 9 for Exiting!");
+
+            int choice = userChoice();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("CREATING BOOKINGGGGGG");
+                    keepAsking = false;
+                    break;
+                case 2:
+                    System.out.println("CREATING GUESSSSST");
+                    keepAsking = false;
+                    break;
+                case 3:
+                    System.out.println("ADDING ROOOOOMMMMMMM");
+                    keepAsking = false;
+                    break;
+                case 4:
+                    System.out.println("ADDING STAFFFFFFFFFFF");
+                    keepAsking = false;
+                    break;
+                case 0:
+                    System.out.println("Please try again.");
+                    break;
+                case 9:
+                    System.out.println("Goodbye! ☺");
+                    System.exit(0);
+                default:
+                    System.out.println("Choose a number from the list!");
+            }
+        }
+
+    }
+
+    public static int userChoice(){
+        Scanner userChoice = new Scanner(System.in);
+        int choice = 0;
+        try {
+            choice = userChoice.nextInt();
+        } catch (InputMismatchException e){
+            System.out.println("Hey man, write a number");
+        }
+        return choice;
+    }
 
 }

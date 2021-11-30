c30783a9ff26692d5a8640fc2fdd9f0dbc418272
NicolasJavierJan <nicolasjavierjan@gmail.com>
2021-11-30 13:08:35 +0100

    Changed Booking

--- src/Booking.java
+++ src/Booking.java
@@ -1,31 +1,35 @@
 public class Booking {
     // Bookings (start date, end date, room number, Guest)
-    private int startDate;
-    private int endDate;
+    private int startDateDay;
+    private String startDateMonth;
+    private int endDateDay;
+    private String endDateMonth;
     private int numberOfNights;
     private Room room;
 
-    public Booking(int startDate, int endDate, int numberOfNights) {
-        this.startDate = startDate;
-        this.endDate = endDate;
+    public Booking(int startDateDay, String startDateMonth, int endDateDay, String endDateMonth, int numberOfNights) {
+        this.startDateDay = startDateDay;
+        this.startDateMonth = startDateMonth;
+        this.endDateDay = endDateDay;
+        this.endDateMonth = endDateMonth;
         this.numberOfNights = numberOfNights;
     }
 
 
     public int getStartDate() {
-        return startDate;
+        return startDateDay;
     }
 
     public void setStartDate(int startDate) {
-        this.startDate = startDate;
+        this.startDateDay = startDate;
     }
 
-    public int getEndDate() {
-        return endDate;
+    public int gtEndDate() {
+        return endDateDay;
     }
 
     public void setEndDate(int endDate) {
-        this.endDate = endDate;
+        this.endDateDay = endDate;
     }
 
     public int getNumberOfNights() {
@@ -42,5 +46,37 @@ public class Booking {
 
     public void setRoom(Room room) {
         this.room = room;
+    }
+
+    public int getStartDateDay() {
+        return startDateDay;
+    }
+
+    public void setStartDateDay(int startDateDay) {
+        this.startDateDay = startDateDay;
+    }
+
+    public String getStartDateMonth() {
+        return startDateMonth;
+    }
+
+    public void setStartDateMonth(String startDateMonth) {
+        this.startDateMonth = startDateMonth;
+    }
+
+    public int getEndDateDay() {
+        return endDateDay;
+    }
+
+    public void setEndDateDay(int endDateDay) {
+        this.endDateDay = endDateDay;
+    }
+
+    public String getEndDateMonth() {
+        return endDateMonth;
+    }
+
+    public void setEndDateMonth(String endDateMonth) {
+        this.endDateMonth = endDateMonth;
     }
 }
